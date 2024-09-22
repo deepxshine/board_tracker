3166fc6f14962dfe900d0b5c5fc0d77d04b909da
deepxshine <aevdok2800@gmail.com>
2024-09-22 23:42:03 +0300

    feature: add ci

--- backend/src/database/database.py
+++ backend/src/database/database.py
@@ -1,11 +1,13 @@
-from sqlalchemy.ext.asyncio import (AsyncSession, create_async_engine,
-                                    async_sessionmaker)
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
 from ..constants import (POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB,
                          POSTGRES_PORT, POSTGRES_HOST
                          )
 
 postgres_url = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"  # noqa
+
+engine = create_async_engine(postgres_url)
 
 
 async def get_session() -> AsyncSession:
--- docs/HISTORY.md
+++ docs/HISTORY.md
@@ -2,4 +2,3 @@
 |---------|----------------------------------------|------------|------------------|
 | 0.0.1   | Backend: Add basic models              | 21.09.2024 | Artem Gavrilov   |  
 | 0.0.2   | Backend: add alembic, db comnn, docker | 22.09.2024 | Alexey Evdokimov |
-
