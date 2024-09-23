40d0c5f554e9bf88730a4e124560478b73b7c521
kaiineran <renaikaii@mail.ru>
2024-09-24 00:40:51 +0300

    schemas

--- backend/src/schemas/inventory_in_ticket.py
+++ backend/src/schemas/inventory_in_ticket.py
@@ -1,13 +1,8 @@
-from fastapi import FastAPI
 from pydantic import BaseModel
 
-class InventoryInTicket(BaseModel):
-    id:int
-    inventory_id:int
-    ticket_id:int
 
-app = FastAPI()
+class InventoryInTicketOutSchema(BaseModel):
 
-@app.post("/inventory_in_ticket/", response_model=InventoryInTicket)
-async def create_inventory_in_ticket(inventory_in_ticket: InventoryInTicket):
-    return inventory_in_ticket
+    id: str
+    inventory_id: str
+    ticket_id: str
